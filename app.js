debed2f091c67d455e7a0d6bec2446e9346c815b
Vladyslav <v.royf@yahoo.com>
2022-01-29 19:30:55 +0200

    CW

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+/********************* Objects  ***********/
 
-/*
-const ar20 = [13, 17, 20, 23, 2, 40]
-// const arEvenOdd = ar20.filter((n,i,a) => a.length % 2 == 0 ? n % 2 == 0 : n % 2 == 1);
-// console.log(arEvenOdd); 
+const person1 = {id: 123, name: 'Moshe', address: {city: 'Lod', street: 'Sokolov'}};
+const person2 = {id: 123, name: 'Moshe', address: {city: 'Lod', street: 'Sokolov'}};
+console.log(`person1 == person2 is ${person1 == person2}`);   // false потому что разные объекты, хоть и с одинаковыми значениями
+console.log(`"123" == 123 is ${"123"==123}`);                // true потому что дабл= сравнивает только значения 
+console.log(`"123" === 123 is ${"123"===123}`);             // false потому что 3= сравнивает и значения и тип данных ("123" - это стринг, 123 - намбер)
+
+// рекомендуется использовать 3=
+
+console.log(`JSON.stringify(person1) === JSON.stringify(person2) is ${JSON.stringify(person1) === 
+        JSON.stringify(person2)}`);
+
+console.log(JSON.stringify(person1));
+console.log(`name of person1 is ${person1.name}`);
+console.log(`person1 lives in city ${person1.address.city}`);
+Object.keys(person1).forEach(k => console.log(k));                // Фунцкия для получения ключей объекта (и их распечатка). Ключи в данном случае: ай-ди, имя, адресс
+Object.values(person1).forEach(v => console.log(v))              // Функция для получения значений ключей
+Object.entries(person1).forEach(e => console.log(e))            // Массив масивов (или ключ + его значение данного объекта)
+
+function createPerson(id, name, address) {
+    return {id, name, address}
+}
+function createAddress(city, street) {
+    //return {city: city, street: street} <=> return {city, street}     Оба варианта рабочие
+    return {city, street}
+}
+const persons = [
+    createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
+    createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
+    createPerson(123, "Kolya", createAddress("Tel-Aviv","Dizengoff"))
+]
 
 
-const res = ar20.reduce((res, cur) => res + cur, 0);
-console.log(res)
-const max = ar20.reduce((max , cur) => cur > max ? cur : max, ar20[0]);
-console.log(max)
-*/
 
-const array = [12, 13, 14, 15, 22, 44]
-myForEach(array.forEach(n))
-
-console.log(myForEach);
